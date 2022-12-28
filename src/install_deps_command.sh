bc4bb258d16c04ea6899fcd442e322ebf681109d
siyuan0322 <siyuanzhang.zsy@alibaba-inc.com>
2022-12-28 15:10:40 +0800

    change deps-prefix to from-local

--- src/install_deps_command.sh
+++ src/install_deps_command.sh
@@ -4,7 +4,7 @@ type=${args[type]}
 # from-local=${args[--from-local]}
 cn=${args[--cn]}
 install_prefix=${args[--prefix]}
-deps_prefix=${args[--deps-prefix]}
+deps_prefix=${args[--from-local]}
 
 only_grape_v6d=${args[--only-grape-v6d]}
 no_grape_v6d=${args[--no-grape-v6d]}
