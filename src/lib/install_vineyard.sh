3bcc1f69855ecf8a6c222834bc7e0d894d4ff794
siyuan0322 <siyuanzhang.zsy@alibaba-inc.com>
2023-01-31 13:10:30 +0800

    update

--- src/lib/install_vineyard.sh
+++ src/lib/install_vineyard.sh
@@ -5,23 +5,28 @@ install_vineyard() {
   jobs=${4:-4} # $4:default=4
   V6D_PREFIX="/opt/vineyard"  # fixed, related to coordinator/setup.py
 
-  if command -v vineyardd &> /dev/null && \
-     [[ $(vineyardd --version 2>&1 | awk '{print "v"$3}') == "${v6d_version}" ]]; then
-    log "vineyard ${v6d_version} already installed, skip."
+  if [[ -f "${V6D_PREFIX}/include/vineyard/client/client.h" ]]; then
+    log "vineyard already installed, skip."
     return 0
   fi
 
   directory="v6d"
-  branch=${v6d_version}
-  file="${directory}-${branch}.tar.gz"
-  url="https://github.com/v6d-io/v6d.git"
-  url=$(maybe_set_to_cn_url ${url})
+  file="${directory}-${v6d_version}.tar.gz"
   log "Building and installing ${directory}."
   pushd "${workdir}" || exit
-  if [[ ${url} == *.git ]]; then
-    clone_if_not_exists ${directory} "${file}" "${url}" "${branch}"
+  if [[ "${v6d_version}" == "v"* ]]; then
+    url="https://github.com/v6d-io/v6d.git"
+    clone_if_not_exists ${directory} "${file}" "${url}" "${v6d_version}"
   else
-    download_tar_and_untar_if_not_exists ${directory} "${file}" "${url}"
+    url="https://github.com/v6d-io/v6d/releases/download/${v6d_version}"
+    cn_url=$(maybe_set_to_cn_url "${url}")
+    status=$(curl --head --silent "${cn_url}"/"${file}" | head -n 1)
+    if echo "${status}" | grep -q 404; then
+      download_tar_and_untar_if_not_exists ${directory} "${file}" "${url}"
+    else
+      download_tar_and_untar_if_not_exists ${directory} "${file}" "${cn_url}"
+    fi
+
   fi
   pushd ${directory} || exit
 
@@ -30,7 +35,7 @@ install_vineyard() {
         -DBUILD_VINEYARD_TESTS=OFF \
         -DBUILD_SHARED_LIBS=ON \
         -DBUILD_VINEYARD_PYTHON_BINDINGS=ON
-  make -j${jobs}
+  make -j"${jobs}"
   make install
   strip "${V6D_PREFIX}"/bin/vineyard* "${V6D_PREFIX}"/lib/libvineyard*
   python3 setup.py bdist_wheel
