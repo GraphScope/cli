b9f5c108b68d501f66acf83299479a9f6bf830f7
siyuan0322 <siyuanzhang.zsy@alibaba-inc.com>
2023-05-26 10:52:00 +0800

    dev

--- src/dev_command.sh
+++ src/dev_command.sh
@@ -1,17 +1,20 @@
 inspect_args
 
 local=${args[--local]}
+
+mount_option=""
+
 if [[ -n $local ]]; then
-    echo "local=" $local
+    echo "Opened a new container with $local mounted to /home/graphscope/graphscope."
 else
-    echo "No local assigned, use default `pwd` to mount as working directory."
-    local=`pwd`
+    echo "No local directory assigned, open a new container without mounting local directory."
+    mount_option="--mount type=bind,source="${local},target=/home/graphscope/graphscope"
 fi
 
 #docker pull graphscope/graphscope-dev
+REGISTRY=registry.cn-hongkong.aliyuncs.com
 docker run \
-    -it graphscope/graphscope-dev \
-    /bin/bash
+    -it \
+    ${mount_option} \
+    ${REGISRY}/graphscope/graphscope-dev:latest
 
-
-    # -v $local:/home/graphscope/workspace 
